6c92e0ae3cc265f0e7567ec27a682f5055c16aa3
Gustavo Gomez <gustavogomez092@gmail.com>
2025-05-04 20:46:38 -0600

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import partytown from '@astrojs/partytown';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [partytown(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
