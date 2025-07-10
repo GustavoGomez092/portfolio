5d37a8a6908c0774177188ea7acf914c9a2990c7
Gustavo Gomez <gustavogomez092@gmail.com>
2025-07-09 20:45:03 -0700

    :fire: finished animation and header with crude responsiveness

--- src/stores/introStore.ts
+++ src/stores/introStore.ts
@@ -1,6 +1,25 @@
-import { atom } from "nanostores"
+import { persistentAtom, setPersistentEngine } from "@nanostores/persistent"
 
-export const $introComplete = atom<Boolean>(false)
+export const $introComplete = persistentAtom<Boolean>("introShown", false, {
+  encode: JSON.stringify,
+  decode: JSON.parse,
+})
+
+let listeners: Array<(...args: any[]) => void> = []
+
+const events = {
+  addEventListener(key: string, callback: (...args: any[]) => void) {
+    listeners.push(callback)
+  },
+  removeEventListener(key: string, callback: (...args: any[]) => void) {
+    listeners = listeners.filter((i) => i !== callback)
+  },
+  // window dispatches "storage" events for any key change
+  // => One listener for all map keys is enough
+  perKey: false,
+}
+
+setPersistentEngine(sessionStorage, events)
 
 export function setIntroComplete(complete: Boolean): void {
   $introComplete.set(complete)
