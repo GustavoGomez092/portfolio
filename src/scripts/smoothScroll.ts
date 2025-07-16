7debb41a969ba49a41a2467f9e8e22f52b3ce035
Gustavo Gomez <gustavogomez092@gmail.com>
2025-07-15 21:43:11 -0700

    :fire: finished hero animation and responsive version, working on about section and animations

--- src/scripts/smoothScroll.ts
+++ src/scripts/smoothScroll.ts
@@ -1,22 +1,17 @@
-import Lenis from "lenis"
 import { gsap } from "gsap"
+import { ScrollTrigger, ScrollSmoother } from "gsap/all"
 import { setSmoothscroll } from "../stores/smoothScrollStore"
 
 // Initializes smooth scrolling with Lenis.
 // Function to set up smooth scrolling.
-export const initSmoothScrolling = () => {
-  // Initialize Lenis for smooth scroll effects. Lerp value controls the smoothness.
-  const lenis = new Lenis({ lerp: 0.15 })
+export const initSmoothScrolling = (): void => {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
-  // Ensure GSAP animations are in sync with Lenis' scroll frame updates.
-  gsap.ticker.add((time) => {
-    lenis.raf(time * 1000) // Convert GSAP's time to milliseconds for Lenis.
+  const smooth = ScrollSmoother.create({
+    smooth: 1,
+    effects: true,
+    normalizeScroll: true,
   })
 
-  // Turn off GSAP's default lag smoothing to avoid conflicts with Lenis.
-  gsap.ticker.lagSmoothing(0)
-
-  setSmoothscroll(lenis)
-
-  return lenis
+  setSmoothscroll(smooth)
 }
